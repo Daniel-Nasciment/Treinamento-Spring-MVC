0046a9ba5e452ab761f599107cda0aed3995f307
Daniel <daniel.fecaf@outlook.com>
2022-03-12 17:20:39 -0300

    Projeto Base

---
+++ src/test/java/br/com/alura/mvc/SpringData1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringData1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
