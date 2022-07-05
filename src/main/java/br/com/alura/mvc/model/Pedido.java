1610751aba6212c58d36e6645e734e55b19c1d61
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-04 22:03:20 -0300

    Ajustando layout home.html | Mapeando entidade do banco de dados

--- src/main/java/br/com/alura/mvc/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/model/Pedido.java
@@ -3,14 +3,47 @@ package br.com.alura.mvc.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PEDIDOS")
 public class Pedido {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "NOME_PRODUTO")
 	private String nomeProduto;
+
+	@Column(name = "URL_IMAGEM")
 	private String urlImagem;
+
+	@Column(name = "URL_PRODUTO")
 	private String urlProduto;
+
+	@Column(name = "VALOR_PRODUTO")
 	private BigDecimal valorProduto;
+
+	@Column(name = "DATA_ENTREGA")
 	private LocalDate dataEntrega;
+
+	@Column(name = "DESCRICAO")
 	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getNomeProduto() {
 		return nomeProduto;
