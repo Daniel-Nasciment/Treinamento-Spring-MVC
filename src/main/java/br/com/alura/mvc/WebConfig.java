198879b5b810745d5408bf7e0d9ce61bf22d7071
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-19 17:04:00 -0300

    Removendo anotação @EnableSpringDataWebSupport que não é necessária

--- src/main/java/br/com/alura/mvc/WebConfig.java
+++ src/main/java/br/com/alura/mvc/WebConfig.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -12,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 import br.com.alura.mvc.interceptor.InterceptorAccess;
 
 @Configuration
-@EnableSpringDataWebSupport
 public class WebConfig extends WebMvcConfigurationSupport{
 
 	// É NECESSÁRIO ESSA CLASSE PARA "HABILITAR O INTERCEPTOR"
