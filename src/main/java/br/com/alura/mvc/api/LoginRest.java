b971a9be1cec8fa91e9c3316d1712104e9cc9561
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-20 12:23:04 -0300

    Iniciando a reconfiguração do spring security

---
+++ src/main/java/br/com/alura/mvc/api/LoginRest.java
@@ -0,0 +1,10 @@
+package br.com.alura.mvc.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/login")
+public class LoginRest {
+
+}
