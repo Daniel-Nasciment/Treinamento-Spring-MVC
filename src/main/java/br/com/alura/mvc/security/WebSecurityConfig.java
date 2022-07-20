d10fa108cd5de3c7e12fb661cb2ade9ef0a39eee
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-20 13:09:07 -0300

    Implementando lógica de autenticação com service

--- src/main/java/br/com/alura/mvc/security/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/security/WebSecurityConfig.java
@@ -1,21 +1,30 @@
 package br.com.alura.mvc.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.alura.mvc.service.AuthenticationService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private AuthenticationService authenticationService;
+	
 	// REFAZENDO CONFIGURAÇÕES DE SEGURANÇA PARA UTILIZAÇÃO DO JWT FOCANDO NOS CONTROLLERS REST
 	
 	// CONFIGURAÇÕES DE AUTENTICAÇÃO
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// ESSE MÉTODO userDetailsService EXIGE UM SERVIÇO QUE IMPLEMENTE A INTERFACE UserDetailsService
+		auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 
 	
