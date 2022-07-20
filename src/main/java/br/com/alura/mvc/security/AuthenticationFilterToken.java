cd34059f6529d876e5b066172fdfc79b7a32a0d6
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-20 16:43:32 -0300

    Comentario sobre Authentication Manager

--- src/main/java/br/com/alura/mvc/security/AuthenticationFilterToken.java
+++ src/main/java/br/com/alura/mvc/security/AuthenticationFilterToken.java
@@ -19,12 +19,13 @@ import br.com.alura.mvc.repository.UserRepository;
 public class AuthenticationFilterToken extends OncePerRequestFilter {
 
 	// COMO ESSE CARA NÃO É UM BEAN NÃO É POSSIVEL USAR AUTOWIRED
-	// POREM ADIONANDO ELE A UM CONSTRUTUOR, A CLASSE QUE INSTANCIA AuthenticationFilterToken É UM BEAN DO SPRING
+	// POREM ADIONANDO ELE A UM CONSTRUTUOR, A CLASSE QUE INSTANCIA
+	// AuthenticationFilterToken É UM BEAN DO SPRING
 	// LA SIM PODEREMOS INJETAR O TokenService
 	private TokenService tokenService;
-	
+
 	private UserRepository userRepository;
-	
+
 	public AuthenticationFilterToken(TokenService tokenService, UserRepository userRepository) {
 		this.tokenService = tokenService;
 		this.userRepository = userRepository;
@@ -37,49 +38,51 @@ public class AuthenticationFilterToken extends OncePerRequestFilter {
 
 		// AUTENTICAÇÃO STATELESS NÃO EXISTE MAIS O CONCEITO DE USUÁRIO LOGADO
 		// ENTÃO TODA REQUISIÇÃO SERÁ NECESSÁRIO A AUTENTICAÇÃO
-		
-		
+
 		String tokenRecovered = tokenRecover(request);
-		
-		
+
 		boolean valid = tokenService.validate(tokenRecovered);
-		
-		if(valid) {
+
+		if (valid) {
 			authenticate(tokenRecovered);
 		}
-		
+
 		// JÁ FIZ TUDO O QUE PRECISAVA E A REQUISIÇÃO PODE SEGUIR
 		filterChain.doFilter(request, response);
-		
+
 	}
 
 	private void authenticate(String tokenRecovered) {
-		
+
 		// RECUPERAR ID DO USUÁRIO DENTRO DO TOKEN
 		Integer idUser = tokenService.getUser(tokenRecovered);
-		
+
 		// BUSCO O USUARIO
 		Optional<User> userOptional = userRepository.findById(idUser);
-		
+
 		User user = userOptional.get();
 
-		// O PARAMETRO NULL SERIA O PASSWORD, PORÉM JA FOI PASSADO NO MOMENTO DE AUTENTICAÇÃO
-		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-		
+		// O PARAMETRO NULL SERIA O PASSWORD, PORÉM JA FOI PASSADO NO MOMENTO DE
+		// AUTENTICAÇÃO
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null,
+				user.getAuthorities());
+
+		// A classe AuthenticationManager deve ser utilizada apenas na lógica de autenticação via username/password, para a geração do token.
+
 		// AQUI EU FORÇO O SPRING A VALIDAR O TOKEN/AUTENTICAÇÃO
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-		
+
 	}
 
 	private String tokenRecover(HttpServletRequest request) {
 		String token = request.getHeader("Authorization");
-		
-		if(token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+
+		if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
 			return null;
 		}
-	
+
 		return token.substring(7, token.length());
-		
+
 	}
-	
+
 }
