b01436710b84e29fe613f01ec79ffd1ea2c8e6bb
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-21 12:39:35 -0300

    Trabalhando com profiles

--- src/main/java/br/com/alura/mvc/api/LoginRest.java
+++ src/main/java/br/com/alura/mvc/api/LoginRest.java
@@ -3,6 +3,7 @@ package br.com.alura.mvc.api;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,6 +20,7 @@ import br.com.alura.mvc.security.TokenService;
 
 @RestController
 @RequestMapping(value = "/api/login")
+@Profile("qa")
 public class LoginRest {
 	
 	// O SPRING NÃO CONSGUE INJETAR ESSE CARA, ENTÃO FAREI ISSO NA CLASSE WEBSECURITYCONFIG
