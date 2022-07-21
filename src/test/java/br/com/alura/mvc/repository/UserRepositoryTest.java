40ece1d5b8c7f05f0de479cb3a79fd90974104eb
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-21 13:35:11 -0300

    Fazendo testes em um outro banco de dados com outras configurações

--- src/test/java/br/com/alura/mvc/repository/UserRepositoryTest.java
+++ src/test/java/br/com/alura/mvc/repository/UserRepositoryTest.java
@@ -10,20 +10,33 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import br.com.alura.mvc.model.User;
 
 @DataJpaTest // CRIAR CLASSE DE TESTE PARA REPOSITORY
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // O TESTE RODA NO MESMO BANCO DE DADOS DA APLICAÇÃO
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // ISSO SERVE PARA O SPRING NÃO SUBSTITUIR MINHAS CONFIGURAÇÕES DO DATABASE
+@ActiveProfiles(value = "test") // OUTRA FORMA DE DEFINIR O PROFILE
 public class UserRepositoryTest {
+	
+	// POR PADRÃO O SPRING BOOT CONSIDERA QUE NOS TESTES DE REPOSITORY ESTAREMOS UTILIZANDO UM BANCO DE DADOS EM MEMÓRIA
 
 	@Autowired
 	private UserRepository repository;
+	
+	@Autowired // INICIALIZAR O BANCO DA FORMA QUE EU PRECISE
+	private TestEntityManager em;
 	
 	@Test
 	void deveriaCarregarUmUserAoBuscarPeloNome() {
 		
 		String nome = "daniel";
+		
+		User newUser = new User();
+		newUser.setUsername("daniel");
+		
+		em.persist(newUser);
 		
 		User user = repository.findByUsername(nome).get();
 		
